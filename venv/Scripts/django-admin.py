2dc07324efe261247946bd9fd9fcc4e13ff842ca
Carol Pan <carolpan@mit.edu>
2019-03-26 11:01:52 -0400

    django setup

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\choco\Documents\GitHub\dedicated_corp_comparison\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
